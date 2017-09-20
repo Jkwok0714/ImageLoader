3eee705bdbdd2ffc826c7eb6db52a831ab6fd527
Justin Kwok <jkwok0714@gmail.com>
2017-09-19 19:35:33 -0700

    Will download from array of links, create dir if needed

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,6 +2,39 @@ var path = require('path');
 var url = require('url');
 var fs = require('fs');
 
-exports.getLink = function (url) {
-  
+exports.getFilename = function(url) {
+  url = url.substring(url.lastIndexOf("/")+ 1);
+  return (url.match(/[^.]+(\.[^?#]+)?/) || [])[0];
+};
+
+exports.getDownload = function(url, outPath) {
+  return new Promise ((resolve, reject) => {
+    const lib = url.startsWith('https') ? require('https') : require('http');
+    //Exit out of helpers folder
+    outPath = '/../' + outPath + exports.getFilename(url);
+    var output = fs.createWriteStream(outPath);
+    console.log('Output path:', outPath);
+    var req = lib.get(url, (res) => {
+      // if (res.statusCode === 200) {
+      console.log('Status code:', res.statusCode); //200 if success
+      res.pipe(output);
+      output.on('finish', () => {
+        output.close();
+        resolve();
+      });
+      // console.log('No 200 status code. Got', res.statusCode);
+      req.setTimeout(10000, () => {
+        req.abort();
+      });
+    });
+
+    req.on('error', (err) => reject(err));
+  });
+};
+
+exports.checkDir = function(targetDir) {
+  if (!fs.existsSync(targetDir)){
+    console.log('dir doesn\'t exist. making:', targetDir);
+    fs.mkdirSync(targetDir);
+  }
 }
--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 var Promise = require('bluebird');
 var fs = require('fs');
-var http = require('http');
-var https = require('https');
+// var http = require('http');
+// var https = require('https');
+var helpers = require('./helpers/helpers');
 
-var url = 'https://boastr.net/wp-content/uploads/2014/11/bttintro2x.jpg';
-var output = fs.createWriteStream(__dirname + '/DL/picture.jpg');
-var req = https.get(url, (res) => {
-  // if (res.statusCode === 200) {
-  res.pipe(output);
-  output.on('finish', () => {
-    output.close();
-  });
-  // }
-  // console.log('No 200 status code. Got', res.statusCode);
-  req.setTimeout(10000, () => {
-    req.abort();
-  });
-});
+// var url = 'https://images-na.ssl-images-amazon.com/images/I/81A3eCGlXkL._SL500_.jpg';
+var targetFolder = '/DL/batchTest3/';
+var urls = ['https://images-na.ssl-images-amazon.com/images/I/81A3eCGlXkL._SL500_.jpg',
+'https://udemy-images.udemy.com/course/750x422/394968_538b_7.jpg',
+'https://iso.500px.com/wp-content/uploads/2016/03/pedroquintela.jpg',
+'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/123/landscape/electronlogo.png?1499962406',
+'https://download.qnap.com/Origin/i/_index/images/TVS-882ST3_main.png'];
+
+
+
+var asyncMultiDownload = function(arr, targetDir) {
+  var i = 0;
+  var currentItem;
+  helpers.checkDir(targetDir);
+  var loop = function() {
+    if (i >= arr.length) {
+      return;
+    } else {
+      currentItem = arr[i];
+      console.log('Attempting DL:', currentItem);
+      helpers.getDownload(currentItem, targetDir).then((results) => {
+        console.log('DL Done');
+        loop();
+        i++;
+      }).catch((err) => {
+        throw err;
+      });
+    }
+  }
+  loop();
+};
+
+asyncMultiDownload(urls, __dirname + targetFolder);
